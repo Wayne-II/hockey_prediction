f692748e54c0861817bf47656bc34c5b7c7ce150
wayne <wayne.marsh@easygolftour.com>
2022-03-14 16:15:39 -0300

    MVPoC - Minimum Viable Proof of Concept.  Added skater selections per available choice ( 3 ).  refactored code to lift state up.  requires significant refactoring for copy pasted quick n dirty jobs.   Dey tuk errr jerbs...dirk dirk derrr

--- hockey_prediction_app/src/App.js
+++ hockey_prediction_app/src/App.js
@@ -1,19 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
+
+class SelectedSkaters extends React.Component {
+  render(){
+    if( this.props.isLoading ){
+      return "";
+    }
+    console.log( 'selected skaters props', this.props );
+    //let firstChoice = [];
+    let firstChoice = this.props.skaters
+      .filter( skater =>  this.props.selectedSkaters[ 1 ].find( skaterName => skaterName === skater.name ) )
+      .sort( ( a, b ) => b.goals - a.goals );//descending order
+    let secondChoice = this.props.skaters
+      .filter( skater =>  this.props.selectedSkaters[ 2 ].find( skaterName => skaterName === skater.name ) )
+      .sort( ( a, b ) => b.goals - a.goals );//descending order
+    let thirdChoice = this.props.skaters
+      .filter( skater =>  this.props.selectedSkaters[ 3 ].find( skaterName => skaterName === skater.name ) )
+      .sort( ( a, b ) => b.goals - a.goals );//descending order
+
+    return <div style={ { 
+      display: 'flex',
+      flexDirection: 'row'
+     } }>
+      <div>
+        <h3>1st choice</h3>
+        <ul style={ { listStyleType:'none', padding:0, margin:0 } }>
+          {
+            firstChoice.map( skater => <li>{ skater.name }: { skater.goals }</li>)
+          }
+        </ul>
+      </div>
+      <div>
+        <h3>2nd choice</h3>
+        <ul style={ { listStyleType:'none', padding:0, margin:0 } }>
+          {
+            secondChoice.map( skater => <li>{ skater.name }: { skater.goals }</li>)
+          }
+        </ul>
+      </div>
+      <div>
+        <h3>3rd choice</h3>
+        <ul style={ { listStyleType:'none', padding:0, margin:0 } }>
+          {
+            thirdChoice.map( skater => <li>{ skater.name }: { skater.goals }</li>)
+          }
+        </ul>
+      </div>
+    </div>;
+  }
+}
 
 class Hockey extends React.Component {
-  componentDidMount(){
-    fetch( 'https://hockey/tims/',{mode:'cors'} )
-    .then( response => response.json() )
-    .then( data => {
-    
-      this.setState( data );
-    } );
-  }
-
   getTeamSkaters( team ){
-    var ret = this.state.skaters.filter( skater => {
+    var ret = this.props.skaters.filter( skater => {
       return skater.team === team;
     } );
     return ret;
@@ -21,65 +61,141 @@ class Hockey extends React.Component {
 
   getScheduledTeams(){
     let scheduled_teams = [];
-    scheduled_teams = scheduled_teams.concat( ...this.state.schedule.map( scheduled_match => scheduled_match.split( '-' ) ) );
+    scheduled_teams = scheduled_teams.concat( ...this.props.schedule.map( scheduled_match => scheduled_match.split( '-' ) ) );
     return scheduled_teams;
+  }
+
+  getSkaterListItems( team_skaters ){
+    return team_skaters.map( skater => {
+      return <li style={{ listStyleType:'none', display:'flex', alignItems:'flex-start', textAlign:'start'}}>
+        <input type="radio" name={skater.name} value="0" onChange={this.props.changeSelectedSkater} />
+        <input type="radio" name={skater.name} value="1" onChange={this.props.changeSelectedSkater} />
+        <input type="radio" name={skater.name} value="2" onChange={this.props.changeSelectedSkater} />
+        <input type="radio" name={skater.name} value="3" onChange={this.props.changeSelectedSkater} />
+        {skater.goals} - {skater.name}
+      </li>;
+    } );
   }
 
   getSkatersList(){
     //return skaters in descending order based on goals
-    //return this.state.skaters[0].goals ;
+    //return this.props.skaters[0].goals ;
 
     let scheduled_teams = this.getScheduledTeams();
     let skaters_by_team = {};
 
-    Object.keys( this.state.teams ).map( team => skaters_by_team[ team ] = this.getTeamSkaters( team ) );
+    Object.keys( this.props.teams ).map( team => skaters_by_team[ team ] = this.getTeamSkaters( team ) );
     Object.keys( skaters_by_team ).map( team => skaters_by_team[ team ].sort( ( skater1, skater2 ) => skater2.goals - skater1.goals ) );
 
     let skaters_lists = [];
     for( let team_idx = 0; team_idx < scheduled_teams.length; team_idx += 2 ){
-      let first_wins = this.state.standings[ scheduled_teams[ team_idx ] + '-' + scheduled_teams[ team_idx ] ] > 0;
-
+      let first_wins = this.props.standings[ scheduled_teams[ team_idx ] + '-' + scheduled_teams[ team_idx ] ] > 0;
+      let match_tie = this.props.standings[ scheduled_teams[ team_idx ] + '-' + scheduled_teams[ team_idx ] ] == 0;
+      console.log( this.props.standings[ scheduled_teams[ team_idx ] + '-' + scheduled_teams[ team_idx ] ] );
+      let win_colour = match_tie ? "yellow" : "green";
+      let lose_colour = match_tie ? "yellow" : "red";
       skaters_lists.push(
-        <div>
-          {scheduled_teams[ team_idx ] } @ {scheduled_teams[ team_idx + 1 ] }
-          <br />
-          <span style={ first_wins ? {color:"green"} : {color:"red"}}>{scheduled_teams[ team_idx ] }</span>
-          <ul>
-            { 
-              skaters_by_team[ scheduled_teams[ team_idx ] ].map( skater => <li>{skater.goals} - {skater.name}</li> )
-            }
-          </ul>
-          <span style={ first_wins ? {color:"red"} : {color:"green"}}>{scheduled_teams[ team_idx + 1 ] }</span>
-          <ul>
-            { 
-              skaters_by_team[ scheduled_teams[ team_idx + 1 ] ].map( skater => <li>{skater.goals} - {skater.name}</li> )
-            }
-          </ul>
+        <div style={{width:"33%",display:'flex', flexDirection:'column' } }>
+          <span>{scheduled_teams[ team_idx ] } @ {scheduled_teams[ team_idx + 1 ] }</span>
+          
+          <div style={{display:'flex', flexDirection:'row', flexWrap:'wrap'}}>
+            <span style={ first_wins ? {color:win_colour, width:'50%'} : {color:lose_colour, width:'50%'}}>{scheduled_teams[ team_idx ] }</span>
+            <span style={ first_wins ? {color:lose_colour, width:'50%'} : {color:win_colour, width:'50%'}}>{scheduled_teams[ team_idx + 1 ] }</span>
+            <span style={ { width:'100%', display:'flex', flexWrap: 'no-wrap' } }>
+              <span style={ { width:'50%', display:'flex', alignItems:'flex-start' } }>
+                <span style={ { width: '1.4em' } }>0</span>
+                <span style={ { width: '1.4em' } }>1</span>
+                <span style={ { width: '1.4em' } }>2</span>
+                <span style={ { width: '1.4em' } }>3</span>
+              </span>
+              <span style={ { width:'50%', display:'flex', alignItems:'flex-start' } }>
+                <span style={ { width: '1.4em' } }>0</span>
+                <span style={ { width: '1.4em' } }>1</span>
+                <span style={ { width: '1.4em' } }>2</span>
+                <span style={ { width: '1.4em' } }>3</span>
+              </span>
+            </span>
+            <ul style={{display:"flex", flexDirection:"column",width:'50%', padding:0, margin:0, alignItems:"flex-start", justifyContent:"flex-start"}}>
+              { 
+                this.getSkaterListItems( skaters_by_team[ scheduled_teams[ team_idx ] ] )
+              }
+            </ul>
+            <ul style={{display:"flex", flexDirection:"column",width:'50%', padding:0, margin:0, alignItems:"flex-start", justifyContent:"flex-start"}}>
+              { 
+                this.getSkaterListItems( skaters_by_team[ scheduled_teams[ team_idx + 1 ] ] )
+              }
+            </ul>
+          </div>
         </div>
       );
     }
 
-    return skaters_lists;
+    return <div style={{display:'flex', flexDirection:'row', flexWrap:'wrap'}}>{skaters_lists}</div>;
   }
 
   render(){
-    if( this.state === null ){
+    if( this.props.isLoading ){
       return 'loading';
     }
-    console.log( this.state );
     let skaters_list = this.getSkatersList();
-    return skaters_list;
+    return <div>
+      <span>skater picks</span>
+      {skaters_list}
+      </div>;
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Hockey />
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor( props ){
+    super( props );
+    this.state = { selected_skaters: { 1:[], 2: [], 3:[] }, isLoading: true };
+    this.changeSelectedSkater = this.changeSelectedSkater.bind( this );
+  }
+
+  componentDidMount(){
+    fetch( 'https://hockey/tims/',{mode:'cors'} )
+    .then( response => response.json() )
+    .then( data => {
+      console.log( 'data', data );
+      this.setState( { ...data, isLoading: false } );
+    } );
+  }
+
+  changeSelectedSkater( event ){
+
+    console.log( 'App', event, this, event.target.value );
+    
+    let selection = parseInt( event.target.value );
+    let selection_filter = skater_name => event.target.name !== skater_name;
+    let selected_skaters = {
+      1: [ ...this.state.selected_skaters[ 1 ] ].filter( selection_filter ),
+      2: [ ...this.state.selected_skaters[ 2 ] ].filter( selection_filter ),
+      3: [ ...this.state.selected_skaters[ 3 ] ].filter( selection_filter )
+    };
+
+    if( selection !== 0 ){
+      selected_skaters[ selection ].push( event.target.name );
+    }
+
+    console.log( this.state, selected_skaters );
+    this.setState( { selected_skaters } );
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Coffee-o-Matic</h1>
+          <SelectedSkaters isLoading={ this.state.isLoading } skaters={ this.state.skaters } selectedSkaters={ this.state.selected_skaters } />
+          <Hockey isLoading={ this.state.isLoading } schedule={ this.state.schedule } skaters={ this.state.skaters } standings={ this.state.standings } teams={ this.state.teams } changeSelectedSkater={ this.changeSelectedSkater } />
+        </header>
+        
+      </div>
+    );
+  }
 }
+// function App() {
+  
+// }
 
 export default App;
