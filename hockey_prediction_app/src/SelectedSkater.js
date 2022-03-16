b8a08d72f02da6fcdae68fb269529fdf72fc53e1
wayne <wayne.marsh@easygolftour.com>
2022-03-16 01:32:40 -0300

    adding selected skater component

---
+++ hockey_prediction_app/src/SelectedSkater.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBan } from '@fortawesome/free-solid-svg-icons';
+
+class SelectedSkater extends React.Component{
+    constructor(props){
+        super( props );
+    }
+    render(){
+        console.log( 'selected skater', this.props )
+        return <div>
+            <FontAwesomeIcon 
+                icon={ faBan } 
+                style={ { width:'1em', heigh:'1em' } } 
+                onClick={ this.props.click }
+                data-name={this.props.skater.name} 
+                data-choice="0" 
+            />{ this.props.skater.name } { this.props.skater.goals }
+        </div>;
+    }
+}
+
+export default SelectedSkater;
