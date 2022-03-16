35c5d599b7cf4457eaeecca685c00e47131aef18
wayne <wayne.marsh@easygolftour.com>
2022-03-16 06:49:25 -0300

    renaming player selector to skater selector. refactoring

---
+++ hockey_prediction_app/src/SkaterSelector.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBan, fa1, fa2, fa3 } from '@fortawesome/free-solid-svg-icons';
+import './SkaterSelector.css';
+
+class SkaterSelector extends React.Component{
+    checkSelectedList( selectedList ){
+      return selectedList && selectedList.filter( skaterName => this.props.skater.name === skaterName ).length > 0;
+    }
+
+    //todo: remove inline style and create class
+    generateButtonClass( choice ){
+      let classes = [ 'SkaterSelectorButton_default' ];
+      if( this.checkSelectedList( this.props.selectedSkaters[ choice ] ) ){
+        classes.push( 'SkaterSelectorButton_selected');
+      }
+      return classes;
+    }
+
+    render(){
+      //<input type="radio" name={skater.name} value="0" onChange={this.props.changeSelectedSkater} />
+      let selectors = this.props.removeOnly && this.props.removeOnly === "true" ? [] : [
+        <FontAwesomeIcon icon={ fa1 } class={ this.generateButtonClass( 1 ).join( ' ' ) } onClick={ this.props.click } data-name={this.props.skater.name} data-choice="1" />,
+        <FontAwesomeIcon icon={ fa2 } class={ this.generateButtonClass( 2 ).join( ' ' ) } onClick={ this.props.click } data-name={this.props.skater.name} data-choice="2" />,
+        <FontAwesomeIcon icon={ fa3 } class={ this.generateButtonClass( 3 ).join( ' ' ) } onClick={ this.props.click } data-name={this.props.skater.name} data-choice="3" />
+      ];
+      return <div style={ { display:'flex', flexDirection:'row', padding:0, flex:'1 1 0px', flexWrap:'nowrap', justifyContent:'flex-start' } } >
+        <FontAwesomeIcon icon={ faBan } class={ this.generateButtonClass( 0 ).join( ' ' ) } onClick={ this.props.click } data-name={this.props.skater.name} data-choice="0" />
+        { selectors }
+          {this.props.skater.goals} - {this.props.skater.name}
+      </div>
+    }
+  }
+
+export default SkaterSelector;
