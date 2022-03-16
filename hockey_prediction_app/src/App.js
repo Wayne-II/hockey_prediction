c0901140372067047fdf91a5aedf0be6ec42baf4
wayne <wayne.marsh@easygolftour.com>
2022-03-16 06:48:35 -0300

    refactoring selectionoptions component and adding font to header

--- hockey_prediction_app/src/App.js
+++ hockey_prediction_app/src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
 import SelectedSkaters from './SelectedSkaters.js';
-import PlayerSelector from './PlayerSelector.js';
+import SelectionOptions from './SelectionOptions';
 import QuickAddSearch from './QuickAddSearch.js';
 
 //TODO: replace input buttons with labeled buttons ( 0, 1, 2, 3 being the labels )
@@ -11,82 +11,7 @@ import QuickAddSearch from './QuickAddSearch.js';
 //TODO: extractoin of component(s) to separate files
 //TODO: --extract delete style
 
-class SelectionOptions extends React.Component {
-  getTeamSkaters( team ){//utility function
-    var ret = this.props.skaters.filter( skater => {
-      return skater.team === team;
-    } );
-    return ret;
-  }
 
-  getScheduledTeams(){//utility function
-    let scheduled_teams = [];
-    scheduled_teams = scheduled_teams.concat( ...this.props.schedule.map( scheduled_match => scheduled_match.split( '-' ) ) );
-    return scheduled_teams;
-  }
-
-  
-  getSkaterListItems( team_skaters ){//component(s) generating function
-    return team_skaters.map( skater => {
-      return <li style={{ listStyleType:'none', display:'flex', alignItems:'flex-start', textAlign:'start'}}>
-        <PlayerSelector skater={ skater } selectedSkaters={ this.props.selectedSkaters } click={ this.props.changeSelectedSkater } />
-        {skater.goals} - {skater.name}
-      </li>;
-    } );
-  }
-
-  getSkatersList(){//component(s) generating function
-    let scheduled_teams = this.getScheduledTeams();
-    let skaters_by_team = {};
-
-    Object.keys( this.props.teams ).map( team => skaters_by_team[ team ] = this.getTeamSkaters( team ) );
-    Object.keys( skaters_by_team ).map( team => skaters_by_team[ team ].sort( ( skater1, skater2 ) => skater2.goals - skater1.goals ) );
-
-    let skaters_lists = [];
-    for( let team_idx = 0; team_idx < scheduled_teams.length; team_idx += 2 ){
-      let first_wins = this.props.standings[ scheduled_teams[ team_idx ] + '-' + scheduled_teams[ team_idx ] ] > 0;
-      let match_tie = this.props.standings[ scheduled_teams[ team_idx ] + '-' + scheduled_teams[ team_idx ] ] == 0;
-      let win_colour = match_tie ? "yellow" : "green";
-      let lose_colour = match_tie ? "yellow" : "red";
-      let backgroundColor = team_idx % 4 === 0 ? '#282c34' : '#232a36';
-      skaters_lists.push(
-        <div style={{width:"33%",display:'flex', flexDirection:'column', backgroundColor } }>
-          <span>{scheduled_teams[ team_idx ] } @ {scheduled_teams[ team_idx + 1 ] }</span>
-          
-          <div style={{display:'flex', flexDirection:'row', flexWrap:'wrap'}}>
-            <span style={ first_wins ? {color:win_colour, width:'50%'} : {color:lose_colour, width:'50%'}}>{scheduled_teams[ team_idx ] }</span>
-            <span style={ first_wins ? {color:lose_colour, width:'50%'} : {color:win_colour, width:'50%'}}>{scheduled_teams[ team_idx + 1 ] }</span>
-            <ul onClick={ event => {
-
-            } } style={{display:"flex", flexDirection:"column",width:'50%', padding:0, margin:0, alignItems:"flex-start", justifyContent:"flex-start", flexWrap:'nowrap'}}>
-              { 
-                this.getSkaterListItems( skaters_by_team[ scheduled_teams[ team_idx ] ] )
-              }
-            </ul>
-            <ul style={{display:"flex", flexDirection:"column",width:'50%', padding:0, margin:0, alignItems:"flex-start", justifyContent:"flex-start"}}>
-              { 
-                this.getSkaterListItems( skaters_by_team[ scheduled_teams[ team_idx + 1 ] ] )
-              }
-            </ul>
-          </div>
-        </div>
-      );
-    }
-
-    return <div style={{display:'flex', flexDirection:'row', flexWrap:'wrap'}}>{skaters_lists}</div>;
-  }
-
-  render(){
-    if( this.props.isLoading ){
-      return 'loading';
-    }
-    let skaters_list = this.getSkatersList();
-    return <div>
-      <span>skater picks</span>
-      {skaters_list}
-      </div>;
-  }
-}
 
 class App extends React.Component{
   constructor( props ){
@@ -96,6 +21,7 @@ class App extends React.Component{
   }
 
   componentDidMount(){
+    //todo: separate - might as well use redux for fun
     //fetch app data from API( table scraper )
     fetch( 'https://hockey/tims/',{mode:'cors'} )
     .then( response => response.json() )
@@ -111,7 +37,6 @@ class App extends React.Component{
      * the dataset of the svg element and not the path.  Figure out
      * how to better handle this.
      */
-    console.log( 'change selected', event )
     let dataset = {};
     if( event.target.tagName === 'svg' ){
       dataset = event.target.dataset
@@ -139,7 +64,7 @@ class App extends React.Component{
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Coffee-o-Matic</h1>
+          <h1 style={ { fontFamily: "Coffee Script" } }>Coffee-o-Matic</h1>
           <QuickAddSearch isLoading={ this.state.isLoading } skaters={ this.state.skaters } selectedSkaters={ this.state.selected_skaters } changeSelectedSkater={ this.changeSelectedSkater } />
           <SelectedSkaters isLoading={ this.state.isLoading } skaters={ this.state.skaters } selectedSkaters={ this.state.selected_skaters } changeSelectedSkater={ this.changeSelectedSkater } />
           <SelectionOptions isLoading={ this.state.isLoading } skaters={ this.state.skaters } selectedSkaters={ this.state.selected_skaters } schedule={ this.state.schedule } standings={ this.state.standings } teams={ this.state.teams } changeSelectedSkater={ this.changeSelectedSkater } />
