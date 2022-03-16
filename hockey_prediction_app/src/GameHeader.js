4d966e8f69b5d3691a77070f83f2516f3e318a68
wayne <wayne.marsh@easygolftour.com>
2022-03-16 17:54:43 -0300

    horrible interim style logic

--- hockey_prediction_app/src/GameHeader.js
+++ hockey_prediction_app/src/GameHeader.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 class GameHeader extends React.Component {
     render() {
+        const win = {color:'green'};
+        const lose = {color:'red'};
+        const draw = {color:'yellow'};
+        const defaultStyle = {flexGrow:99, alignSelf:'center'};
+        console.log( this.props.standings );
         const teams = this.props.teams.split('-');
+        const { [this.props.teams]: matchupStandings} = this.props.standings;
+        let homeStyle, visitStyle = { ...defaultStyle, ...draw};
+        if( matchupStandings > 0 ){
+            homeStyle = { ...defaultStyle, ...win };
+            visitStyle = { ...defaultStyle, ...lose };
+        }else if( matchupStandings < 0 ){
+            homeStyle = { ...defaultStyle, ...lose };
+            visitStyle = { ...defaultStyle, ...win };
+        }
         return <div style={{
             display:'flex',
             flexDirection:'row',
@@ -10,9 +24,9 @@ class GameHeader extends React.Component {
             height:'2em',
             fontSize:'2em'
         }}>
-            <span style={{flexGrow:99, alignSelf:'center'}}>{teams[0]}</span>
+            <span style={homeStyle}>{teams[0]}</span>
             <span style={{flexGrow:1, alignSelf:'center', }}>@</span>
-            <span style={{flexGrow:99, alignSelf:'center'}}>{teams[1]}</span>
+            <span style={visitStyle}>{teams[1]}</span>
         </div>;
     }
 }
