bf9c71ba85198e239ae29fc6efff5cbe7da5f57e
wayne <wayne.marsh@easygolftour.com>
2022-03-16 01:33:26 -0300

    using selected skater component in skaters picks

--- hockey_prediction_app/src/SkatersPicks.js
+++ hockey_prediction_app/src/SkatersPicks.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import SelectedSkater from './SelectedSkater.js';
 
 class SkatersPicks extends React.Component {
     render(){
         let choices = this.props.skaters
         .filter( skater =>  this.props.selectedSkaters.find( skaterName => skaterName === skater.name ) )
         .sort( ( a, b ) => b.goals - a.goals );
+
         return <div>
             <h3>{ this.props.header }</h3>
             <ul style={ { listStyleType:'none', padding:0, margin:0 } }>
             {
-                choices.map( skater => <li>{ skater.name }: { skater.goals }</li>)
+                choices.map( skater => <SelectedSkater skater={ skater } click={ this.props.click } />)
             }
             </ul>
         </div>
